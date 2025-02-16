512547e7a607d1b41e76aa64228ffc357f002743
malik-nabeel-ahmed <nabeel.ahmed@seecs.edu.pk>
2025-02-16 20:58:43 +0500

    init code commit

---
+++ src/main/java/edu/pafiast/code_refractoring/CodeRefractoringApplication.java
@@ -0,0 +1,13 @@
+package edu.pafiast.code_refractoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeRefractoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeRefractoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pafiast/code_refractoring/CodeRefractoringApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pafiast.code_refractoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeRefractoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
